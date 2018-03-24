f10a904a4d861e3af92b8dea2c57bc19c68218e9
tommylettieri <tommylettieri@gmail.com>
2018-03-23 23:43:08 -0400

    Added a function that will find all files starting at a specific path

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 const crypto = require('crypto');
 
 function sha1(path) {
@@ -20,6 +21,40 @@ function sha1(path) {
   });
 }
 
+function traverse(options) {
+  fs.readdir(options.filePath, function(err, items) {
+    for (var i=0; i<items.length; i++) {
+      let childPath = path.resolve(options.filePath, items[i]);
+      let stats = fs.lstatSync(childPath);
+      if (stats.isDirectory()) {
+        traverse({
+          filePath: childPath,
+          onFile: options.onFile,
+          failure: options.failure
+        });
+      } else if (stats.isFile()) {
+        if(typeof(options.onFile) == 'function') {
+          options.onFile(childPath);
+        }
+      } else {
+        console.log(childPath + " is not a file or directory");
+        if(typeof(options.failure) == 'function') {
+          options.failure(childPath + " is not a file or directory");
+        }
+      }
+    }
+  });
+}
+
 sha1('index.js').then((result) => {
   console.log(result);
 });
+
+let p = "C:\\Users\\Luigi\\Desktop\\temp"
+
+traverse({
+  filePath: p,
+  onFile: function(filePath) {
+    console.log(filePath);
+  }
+})
