fdf6094ba058c4d7609b50793e07e6bdc2826cf2
TommyZ87 <zeldafanatic87@yahoo.com>
2020-01-18 22:28:51 -0500

    Added hash based mapping

--- index.js
+++ index.js
@@ -1,95 +1,19 @@
 const fileUtils = require('./utils/fileUtils');
 const objectUtils = require('./utils/objectUtils');
 
-// fileUtils.sha1('index.js').then((result) => {
-//   console.log(result);
-// });
-//
-// fileUtils.traverse({
-//   filePath: "C:\\Users\\Luigi\\Desktop\\temp\\.git\\COMMIT_EDITMSG",
-//   onFile: function(filePath) {
-//     console.log("File Base Path" + filePath);
-//   },
-//   onDone: function() {
-//     console.log('file done');
-//   }
-// });
-//
-// fileUtils.traverse({
-//   filePath: "C:\\Users\\Luigi\\Desktop\\temp",
-//   onFile: function(filePath) {
-//     console.log("Directory Base Path" + filePath);
-//   },
-//   onDone: function() {
-//     console.log('dir done');
-//   }
-// });
+(async () => {
+  const results = {};
+  await new Promise(resolve => {
+    fileUtils.traverse({
+      filePath: ".",
+      onFile: async (filePath) => {
+        const hash = await fileUtils.sha1(filePath);
+        results[hash] = results[hash] || [];
+        results[hash].push(filePath);
+      },
+      onDone: resolve
+    });
+  });
 
-// let results = {};
-// fileUtils.traverse({
-//   filePath: "C:\\Users\\Luigi\\Desktop\\temp",
-//   onFile: (filePath) => {
-//     fileUtils.sha1(filePath).then((sha1) => {
-//       let key = sha1 + "_" + fileUtils.getFileSize(filePath);
-//       if (!results[key]) {
-//         results[key] = {
-//           childPaths: []
-//         };
-//       }
-//
-//       results[key].childPaths.push(filePath);
-//     });
-//   },
-//   onDone: () => {
-//     console.log(JSON.stringify(results, null, 2));
-//   }
-// });
-
-let a = {
-  a: 'a',
-  b: 'b',
-  s: 's',
-  q: 'q',
-  r: 'r',
-  // z: 'z',
-  arr: ['a', 'b', 'c',],
-  z: {
-    a: 'a',
-    b: 'b',
-    s: 's',
-    q: 'q',
-    r: 'r',
-    // z: 'z',
-    arr: ['a', 'b', 'c', ],
-  },
-}
-//
-let b = {
-  r: 'r',
-  s: 's',
-  a: 'a',
-  q: 'q',
-  b: 'b',
-  // z: 'z',
-  arr: ['a', 'b', 'c',],
-  z: {
-    a: 'a',
-    b: 'b',
-    s: 's',
-    q: 'q',
-    r: 'r',
-    // z: 'z',
-    arr: [ 'c','a', 'b',],
-  },
-}
-
-// console.log(objectUtils.diffObject(
-//   a,
-//   b
-// ));
-
-console.log(objectUtils.diffObject({
-  a:a,
-  b:b,
-  sortArrays: true
-}));
+  console.log(JSON.stringify(results, null, 2));
+})();
