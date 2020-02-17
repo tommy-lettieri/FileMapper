2639b852022c9fd29770f60097facf9fd350b259
TommyZ87 <zeldafanatic87@yahoo.com>
2020-02-16 20:42:21 -0500

    Moved dups to it's own method

--- index.js
+++ index.js
@@ -44,6 +44,23 @@ const diff = async (firstFile, secondFile) => {
   });
   console.log(JSON.stringify(diff, null, 2));
 }
+
+const dups = async (inFilePath) => {
+  if (!inFilePath) {
+    console.error("dups requires a file path to read from");
+    process.exit(1);
+  }
+  const dupsMapString = await fileUtils.readFile(inFilePath);
+  const dupsMap = JSON.parse(dupsMapString);
+  const resultDupsMap = Object.keys(dupsMap)
+  .filter(key => dupsMap[key].length > 2)
+  .reduce((result, value) => {
+    result[value] = dupsMap[value]
+    return result;
+  }, {});
+  console.log(JSON.stringify(resultDupsMap, null, 2));
+}
+
 // arg 0: node exe path
 // arg 1: path to run script
 // arg 2: command
@@ -60,26 +77,14 @@ const diff = async (firstFile, secondFile) => {
   const secondFile = process.argv[4];
   await diff(firstFile, secondFile);
   break;
-    case "dups":
-    const inFilePath = process.argv[3];
-    if (!inFilePath) {
-      console.error("dups requires a file path to read from");
-      process.exit(1);
-    }
-    const dupsMapString = await fileUtils.readFile(inFilePath);
-    const dupsMap = JSON.parse(dupsMapString);
-    const resultDupsMap = Object.keys(dupsMap)
-    .filter(key => dupsMap[key].length > 2)
-    .reduce((result, value) => {
-      result[value] = dupsMap[value]
-      return result;
-    }, {});
-    console.log(JSON.stringify(resultDupsMap, null, 2));
-    break;
-    case "usage":
-    console.log("TODO");
-    break;
-    default:
-    console.log(`Invalid command "${command}"`)
-  }
+  case "dups":
+  const inFilePath = process.argv[3];
+  await dups(inFilePath);
+  break;
+  case "usage":
+  console.log("TODO");
+  break;
+  default:
+  console.log(`Invalid command "${command}"`)
+}
 })();
