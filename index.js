90dffd41f43ec594336d4c8c326bde305085eda0
TommyZ87 <zeldafanatic87@yahoo.com>
2020-02-16 20:29:35 -0500

    Moved diff to it's own function

--- index.js
+++ index.js
@@ -23,6 +23,27 @@ const map = async (startFilePath, outFilePath) => {
     await fileUtils.writeFile(outFilePath, s);
   }
 }
+
+const diff = async (firstFile, secondFile) => {
+  if (!firstFile) {
+    console.error("diff requires a two files to read from, received none");
+    process.exit(1);
+  }
+  if (!secondFile) {
+    console.error("diff requires a two files to read from, received one");
+    process.exit(1);
+  }
+
+  const firstFileString = await fileUtils.readFile(firstFile);
+  const secondFileString = await fileUtils.readFile(secondFile);
+  const firstArray = Object.keys(JSON.parse(firstFileString));
+  const secondArray = Object.keys(JSON.parse(secondFileString));
+  const diff = objectUtils.diffArray({
+    firstArray,
+    secondArray
+  });
+  console.log(JSON.stringify(diff, null, 2));
+}
 // arg 0: node exe path
 // arg 1: path to run script
 // arg 2: command
@@ -32,30 +53,13 @@ const map = async (startFilePath, outFilePath) => {
   case "map":
   const startFilePath = process.argv[3] || ".";
   const outFilePath = process.argv[4];
-  map(startFilePath, outFilePath);
+  await map(startFilePath, outFilePath);
   break;
-    case "diff":
-    const firstFile = process.argv[3];
-    if (!firstFile) {
-      console.error("diff requires a two files to read from, received none");
-      process.exit(1);
-    }
-    const secondFile = process.argv[4];
-    if (!secondFile) {
-      console.error("diff requires a two files to read from, received one");
-      process.exit(1);
-    }
-
-    const firstFileString = await fileUtils.readFile(firstFile);
-    const secondFileString = await fileUtils.readFile(secondFile);
-    const firstArray = Object.keys(JSON.parse(firstFileString));
-    const secondArray = Object.keys(JSON.parse(secondFileString));
-    const diff = objectUtils.diffArray({
-      firstArray,
-      secondArray
-    });
-    console.log(JSON.stringify(diff, null, 2));
-    break;
+  case "diff":
+  const firstFile = process.argv[3];
+  const secondFile = process.argv[4];
+  await diff(firstFile, secondFile);
+  break;
     case "dups":
     const inFilePath = process.argv[3];
     if (!inFilePath) {
